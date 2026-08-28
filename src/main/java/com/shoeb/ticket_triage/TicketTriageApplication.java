d8893becb5f37358be87a391ecb558971471f0e5
SHOEB AHMED <shoebahmed@SHOEBs-MacBook-Air.local>
2026-08-28 23:43:43 +0530

    Initial Spring Boot setup with MySQL config

---
+++ src/main/java/com/shoeb/ticket_triage/TicketTriageApplication.java
@@ -0,0 +1,13 @@
+package com.shoeb.ticket_triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketTriageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoeb/ticket_triage/TicketTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoeb.ticket_triage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketTriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
