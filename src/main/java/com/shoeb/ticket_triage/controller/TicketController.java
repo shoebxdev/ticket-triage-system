5f626699c7feddfb186574f9325c55030c5b9b9d
shoebxdev <shoebahmed031@gmail.com>
2026-08-30 12:18:06 +0530

    Add CORS config to allow React frontend requests

--- src/main/java/com/shoeb/ticket_triage/controller/TicketController.java
+++ src/main/java/com/shoeb/ticket_triage/controller/TicketController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tickets")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TicketController {
 
     @PutMapping("/{id}")
